b18376dbbeb99aee7b9cea7e380bd8d4a829f86e
pteo <57054664+pteo@users.noreply.github.com>
2025-04-06 16:34:18 +0800

    svelte config change

--- svelte.config.js
+++ svelte.config.js
@@ -15,12 +15,11 @@ const config = {
 				throw new Error(message);
 			}
 		}
-
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter()
+	}
+};
