c0602f3b93d314db98ce555f9206a92dc5c86a5e
pteo <57054664+pteo@users.noreply.github.com>
2025-04-07 11:14:25 +0800

    Update svelte.config.js - works on local

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,26 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// prerender: {
-		// 	handleHttpError: ({ path, referrer, message }) => {
-		// 		// ignore deliberate link to shiny 404 page
-		// 		if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-		// 			return;
-		// 		}
-
-		// 		// otherwise fail the build
-		// 		throw new Error(message);
-		// 	}
-		// }
+		adapter: adapter()
+// 		prerender: {
+// 			handleHttpError: ({ path, referrer, message }) => {
+// 				// ignore deliberate link to shiny 404 page
+// 				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+// 					return;
+// 				}
+// 
+// 				// otherwise fail the build
+// 				throw new Error(message);
+// 			}
+// 		}
 	}
 };
 
 export default {
-	kit: {
-		adapter: adapter()
-	}
+
 };
 
 
